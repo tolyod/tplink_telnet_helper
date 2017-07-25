22f4ea755b0d2d700ae2ac5668bda68b2fefa9d8
Anatoliy Poloz <t0ly@ukr.net>
2017-07-25 17:34:01 +0300

    added usage example to example.php

---
+++ example.php
@@ -0,0 +1,18 @@
+<?php
+
+//echo "----- ".$switch_name." ----- \n";
+include_once("tplink_switch_config.php");
+include_once("tplink_switch_lib.php");
+include_once("lib/Net_Telnet/Net/Telnet.php");
+        
+$router='10.22.0.134';
+        
+var_dump(get_tplink_running_config($router));
+var_dump(get_tplink_mac_addresses_raw_str($router));
+var_dump(get_tplink_sys_info($router));
+var_dump(get_tplink_ports_arr($router));
+var_dump(get_tplink_port_short_name($router, 3));
+var_dump(get_tplink_port_macs($router, 1));
+exit();
+
+?>
